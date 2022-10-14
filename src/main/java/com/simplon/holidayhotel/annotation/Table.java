bc7fd6c97f29521c1ee17ce78dffaf37999c3b55
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-10-14 15:52:59 +0100

    build(HOT21): starty building model and repository entities

---
+++ src/main/java/com/simplon/holidayhotel/annotation/Table.java
@@ -0,0 +1,10 @@
+package com.simplon.holidayhotel.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Table {
+    String tableName();
+
+}
--- src/main/java/com/simplon/holidayhotel/models/Client.java
+++
@@ -1,4 +0,0 @@
-package com.simplon.holidayhotel.models;
-
-public class Client {
-}
