7c55aec9f30119c7affbe94d24af969475f1c2fe
Said Oudouane <saidoudouane@gmail.com>
2022-10-15 23:50:18 +0100

    refactor(HOT30): change copyProperties helper method return type to void

--- src/main/java/com/simplon/holidayhotel/utils/Helper.java
+++ src/main/java/com/simplon/holidayhotel/utils/Helper.java
@@ -1,7 +1,7 @@
 package com.simplon.holidayhotel.utils;
 
 public class Helper {
-    public static <T> boolean copyProperties(T source, T target) {
+    public static <T> void copyProperties(T source, T target) {
         try {
             for (java.lang.reflect.Field field : source.getClass().getDeclaredFields()) {
                 field.setAccessible(true);
@@ -9,11 +9,9 @@ public class Helper {
                 field.set(target, value);
                 field.setAccessible(false);
             }
-            return true;
         } catch (Exception e) {
             System.out.println("Error while copying properties");
             e.printStackTrace();
-            return false;
         }
     }
 
