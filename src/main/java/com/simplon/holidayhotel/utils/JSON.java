6e11cd0e9f1d6f0145514ce27d7a7d96d06c52ca
Said Oudouane <saidoudouane@gmail.com>
2022-10-15 19:18:50 +0100

    feat(HOT30): Add parse ability to JSON service

--- src/main/java/com/simplon/holidayhotel/utils/JSON.java
+++ src/main/java/com/simplon/holidayhotel/utils/JSON.java
@@ -2,6 +2,8 @@ package com.simplon.holidayhotel.utils;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+
 public class JSON {
     private static Gson gson;
 
@@ -19,5 +21,9 @@ public class JSON {
 
     public static <T> T parse(String json, Class<T> type) {
         return getGson().fromJson(json, type);
+    }
+
+    public static <T> T parse(BufferedReader reader, Class<T> clazz) {
+        return getGson().fromJson(reader, clazz);
     }
 }
