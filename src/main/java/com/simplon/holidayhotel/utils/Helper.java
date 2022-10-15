0a899f0506adee38c6546d89dc3f21099be29a04
Said Oudouane <saidoudouane@gmail.com>
2022-10-15 19:56:25 +0100

    feat(HOT30): add helper method to copy non empty fields in instances

--- src/main/java/com/simplon/holidayhotel/utils/Helper.java
+++ src/main/java/com/simplon/holidayhotel/utils/Helper.java
@@ -1,7 +1,7 @@
 package com.simplon.holidayhotel.utils;
 
 public class Helper {
-    public static<T> boolean copyProperties(T source, T target) {
+    public static <T> boolean copyProperties(T source, T target) {
         try {
             for (java.lang.reflect.Field field : source.getClass().getDeclaredFields()) {
                 field.setAccessible(true);
@@ -15,5 +15,23 @@ public class Helper {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public static <T> void copyNonEmptyProperties(T source, T target) {
+        try {
+            for (java.lang.reflect.Field field : source.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                Object value = field.get(source);
+                if (value != null && !value.toString().isEmpty() && !value.toString().equals("0") && !value.toString()
+                                                                                                           .equals("0.0")) {
+                    field.set(target, value);
+                }
+                field.setAccessible(false);
+            }
+        } catch (Exception e) {
+            System.out.println("Error while copying properties");
+            e.printStackTrace();
+        }
+
     }
 }
